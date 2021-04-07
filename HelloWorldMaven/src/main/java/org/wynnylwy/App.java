fb36b27fef273b39e1d3187d4b92b3b26c461a98
Wynny Lim <60843862+wynnylwy@users.noreply.github.com>
2021-04-07 11:23:31 +0800

    Add files via upload

---
+++ HelloWorldMaven/src/main/java/org/wynnylwy/App.java
@@ -0,0 +1,21 @@
+package org.wynnylwy;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App(){
+        setTitle("Hello World");
+        setSize(600,400);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+
+    public static void main( String[] args )
+    {
+
+        new App();
+    }
+}
